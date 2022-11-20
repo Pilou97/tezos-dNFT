4a23c6a9d96c75f907fa1e1f88df75a75a97fb72
Pierre-Jean Sauvage <pierrejean.sauvage.etu@univ-lille.fr>
2022-11-20 18:30:35 +0100

    fixup! fixup! add e2e tests

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,12 +12,12 @@ alias ligo="docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.55.0"
 contract=$(ligo compile contract contract/main.mligo)
 storage=$(ligo compile storage contract/main.mligo "Storage.empty ()")
 
-tezos-client --endpoint http://localhost:$PORT originate contract nft transferring 0 from alice running "$contract" --init "$storage" --burn-cap 1 --force
+octez-client --endpoint http://localhost:$PORT originate contract nft transferring 0 from alice running "$contract" --init "$storage" --burn-cap 1 --force
 
 sleep 4
 
-tezos-client --endpoint http://localhost:$PORT transfer 0 from alice to nft --arg '(Left (Left (Right { Elt "latitude" 0x0304990A ; Elt "longitude" 0x002EAB94 })))' --burn-cap 1
+octez-client --endpoint http://localhost:$PORT transfer 0 from alice to nft --arg '(Left (Left (Right { Elt "latitude" 0x0304990A ; Elt "longitude" 0x002EAB94 })))' --burn-cap 1
 
-tezos-client --endpoint http://localhost:$PORT transfer 0 from alice to nft --arg '(Left (Left (Right { Elt "latitude" 0x01 ; Elt "longitude" 0x02 })))' --burn-cap 1
+octez-client --endpoint http://localhost:$PORT transfer 0 from alice to nft --arg '(Left (Left (Right { Elt "latitude" 0x01 ; Elt "longitude" 0x02 })))' --burn-cap 1
 
-tezos-client --endpoint http://localhost:$PORT show known contract nft
+octez-client --endpoint http://localhost:$PORT show known contract nft
